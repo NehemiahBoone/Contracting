98c8f4f839763ecc05cbf1340098910273e35fe3
Nehemiah Boone <nehemiah.boone@gmail.com>
2020-10-19 13:59:55 -0600

    models

---
+++ Models/Contractor.cs
@@ -0,0 +1,11 @@
+namespace Contracting.Models
+{
+  public class Contractor
+  {
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string Contact { get; set; }
+    public string Skills { get; set; }
+    public int Id { get; set; }
+  }
+}
---
+++ Models/Job.cs
@@ -0,0 +1,11 @@
+namespace Contracting.Models
+{
+  public class Job
+  {
+    public string Location { get; set; }
+    public string Description { get; set; }
+    public string Contact { get; set; }
+    public decimal HourlyPay { get; set; }
+    public int Id { get; set; }
+  }
+}
